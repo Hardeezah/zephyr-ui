2454fd0b1d9e6e0b0ab78cfa1d1d165102eeb790
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2024-11-09 10:23:04 +0100

    remove generated files

--- types/components/button/mindspa_button.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export declare class Button extends React.Component<ButtonProps> {
-    constructor(parameters: ButtonProps);
-}
-interface ButtonProps {
-    label: string;
-    onClick: () => void;
-}
-export {};
